8d71e2884aee8eb9bf4ff226919eea8ef5ea6ec4
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2026-02-05 13:12:20 +0600

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by jyotirmoy_halder on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
